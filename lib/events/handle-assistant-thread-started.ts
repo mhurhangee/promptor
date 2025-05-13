30917cadd7bc069700ab6aab52d561c0b368cda0
Michael Hurhangee <m.hurhangee@me.com>
2025-05-13 18:08:01 +0000

    split handle new messages into new message and new thread

---
+++ lib/events/handle-assistant-thread-started.ts
@@ -0,0 +1,15 @@
+import type { AssistantThreadStartedEvent } from '@slack/web-api'
+import { INITIAL_CONTEXT, INITIAL_FOLLOWUPS, WELCOME_MESSAGES } from '../config'
+import { postMessage } from '../slack'
+import { setFollowupsUtil } from '../slack'
+import { getRandomItem, getRandomItems } from '../utils'
+
+export async function handleAssistantThreadStarted(event: AssistantThreadStartedEvent) {
+  const { channel_id, thread_ts } = event.assistant_thread
+
+  // Post welcome message
+  await postMessage(getRandomItem(WELCOME_MESSAGES), INITIAL_CONTEXT, channel_id, thread_ts)
+
+  // Set 3 random initial followups
+  await setFollowupsUtil(channel_id, thread_ts)(getRandomItems(INITIAL_FOLLOWUPS, 3))
+}
